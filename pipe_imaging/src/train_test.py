97d43dbd25824f9a42b382e9fb6d26c02e284e7e
cwibisono <wibisono.catur12@gmail.com>
2026-04-29 17:49:27 +0700

    update on the train_test module

--- pipe_imaging/src/train_test.py
+++ pipe_imaging/src/train_test.py
@@ -5,12 +5,16 @@ if __name__ == "__main__":
     import sys
     import nn
 
+    '''
+    Below is an example how to use module above
+    '''
+
     file_train = sys.argv[1]
     file_test = sys.argv[2]
     file_out = sys.argv[3]
 
     X_train, y_train = nn.get_features(file_train)
-    print(X_train[2], y_train[2])
+    
     model_param = nn.get_parameters(X_train, y_train)
 
     X_test, y_test = nn.get_features(file_test)
