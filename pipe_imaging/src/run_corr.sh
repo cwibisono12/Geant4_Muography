6ff4865207fda010e80ef69dbac27ef0f0793d31
cwibisono <wibisono.catur12@gmail.com>
2026-05-18 14:27:12 +0700

    update on run_corr bash script

--- pipe_imaging/src/run_corr.sh
+++ pipe_imaging/src/run_corr.sh
@@ -6,7 +6,7 @@ directory_src=$1 #initial directory of the hits files
 directory_final=$2 #final directory to store the correlated file
 initial=$3 #Run number being proceseed
 final=$4 #Final Run number being processed
-
+run_mode=$5 #Run mode file (1, 2, 3, or 4)
 
 
 
@@ -14,14 +14,14 @@ if [[ "$initial" -ge 0 &&  "$initial" -lt 10000 ]];
 	then
 		for ((j=initial;j<=final;j++));
 			do
-			if [ -f "$directory_src/Run${j}_nt_ScintillatorHits.csv" ];
+			if [ -f "$directory_src/Run${j}_${run_mode}_nt_ScintillatorHits.csv" ];
 				then
-					./main.py "$directory_src/Run${j}_nt_ScintillatorHits.csv" \
-					"$directory_src/Run${j}_nt_PipeHits.csv" \
-					"$directory_src/Run${j}_nt_ScalingHits.csv"\
-					"$directory_final/corr_file_Run${j}_new.csv"
+					./main.py "$directory_src/Run${j}_${run_mode}_nt_ScintillatorHits.csv" \
+					"$directory_src/Run${j}_${run_mode}_nt_PipeHits.csv" \
+					"$directory_src/Run${j}_${run_mode}_nt_ScalingHits.csv"\
+					"$directory_final/corr_file_Run${j}_${run_mode}_new.csv"
 			else
-				echo "File for Run $j not found in $directory_src"
+				echo "File for Run $j and mode $run_mode not found in $directory_src"
 			fi
 
 		done
