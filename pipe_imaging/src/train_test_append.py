43b4461616e2d4169afedb8fa663a615e22a98f9
cwibisono <wibisono.catur12@gmail.com>
2026-05-26 15:31:48 +0700

    update on nn module to include the 3 hits on objects for the target variables

--- pipe_imaging/src/train_test_append.py
+++ pipe_imaging/src/train_test_append.py
@@ -11,20 +11,23 @@ if __name__ == "__main__":
 
     file_train = sys.argv[1]
     file_test = sys.argv[2]
-    file_out = sys.argv[3]
+    file_out_train = sys.argv[3]
+    file_out_test = sys.argv[4]
+    option = int(sys.argv[5])
 
-    X_train, y_train = nn.get_features_append(file_train)
-    
+    X_train, y_train = nn.get_features_append(file_train, arg_number = option)
+    nn.write_features(file_out_train, y_train)
+
     dim = len(X_train)
     for i in range(dim):
         print(i, X_train[i], y_train[i])
     
     model_param = nn.get_parameters(X_train, y_train)
 
-    X_test, y_test = nn.get_features_append(file_test)
+    X_test, y_test = nn.get_features_append(file_test, arg_number = option)
     
     result = nn.predict_outcome(X_test, model_param)
 
-    nn.write_features(file_out, result)
+    nn.write_features(file_out_test, result)
     nn.plot_results(result)
     
