ecb023bf6da749651e0c3ca2906abbd3e5cf9fb2
cwibisono <wibisono.catur12@gmail.com>
2026-07-07 11:05:20 +0700

    some updates on some files

--- pipe_imaging/src/examples/testing/merge_predict_file.py
+++ pipe_imaging/src/examples/testing/merge_predict_file.py
@@ -5,9 +5,25 @@ if __name__ == "__main__":
     sys.path.append('../..')
     import utilities
 
-    fin = sys.argv[1] #file input to be merged
-    fout = sys.argv[2] #file to append fin to merged file
-    flog = sys.argv[3] #log file to keep the record
+    directory_in = sys.argv[1] #directory input where the prediction files are
+    option = int(sys.argv[2]) #data retrieval mode when running the simulation (eg. 3 for retrieving all hits on the objects)
+    run_mode = int(sys.argv[3]) #data retrieval mode when running the model for training and testing (1 to 9)
+    initial = int(sys.argv[4]) #begin run number to be merged
+    final = int(sys.argv[5]) #final run number to be merged
+    epoch = int(sys.argv[6]) #iteration number when running the model for training
+    fout = sys.argv[7] #file to store the merged data
+    flog = sys.argv[8] #log file to keep the record
 
-    #merge fin into fout:
-    utilities.merge_prediction_file(fin, fout, flog)
+    #Get the list of file name inside directory:
+    file_list = utilities.retrieve_files_in_directory(directory_in)
+
+    for name in file_list:
+        temp = str(name)
+        for k in range(initial, final+1, 1):
+            pattern = str(k)+'_'+str(option)+'_'+str(run_mode)+'_'+str(epoch)
+            
+            #Merge file that meets condition above:
+            if pattern in temp:
+                #Merge temp into fout:
+                utilities.merge_prediction_file(temp, fout, flog)
+
---
+++ pipe_imaging/src/examples/testing/run_merge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+directory_in=$1 #directory input where the prediction files are
+option=$2
+run_mode=$3
+epoch=$4
+directory_out=$5 #directory output to store the merged files
+directory_log=$6 #directory to store the log file
+
+#./merge_predict_file.py $directory_in $option $run_mode 14 15 $epoch $directory_out/corr_file_conc_1_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 43 45 $epoch $directory_out/corr_file_conc_2_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 11 12 $epoch $directory_out/corr_file_conc_3_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 46 48 $epoch $directory_out/corr_file_conc_4_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 49 51 $epoch $directory_out/corr_file_conc_5_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 7 9 $epoch $directory_out/corr_file_conc_6_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 53 54 $epoch $directory_out/corr_file_conc_7_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode	2 3 $epoch $directory_out/corr_file_conc_8_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 55 57 $epoch $directory_out/corr_file_conc_9_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 5 6 $epoch $directory_out/corr_file_conc_10_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 58 60 $epoch $directory_out/corr_file_conc_11_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 61 63 $epoch $directory_out/corr_file_conc_12_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 64 66 $epoch $directory_out/corr_file_conc_13_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 67 69 $epoch $directory_out/corr_file_conc_14_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 71 72 $epoch $directory_out/corr_file_conc_15_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 73 75 $epoch $directory_out/corr_file_conc_16_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 76 78 $epoch $directory_out/corr_file_conc_17_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
+./merge_predict_file.py $directory_in $option $run_mode 80 81 $epoch $directory_out/corr_file_conc_18_${option}_${run_mode}_${epoch}_predict.csv $directory_log/merge_predict_concentric_log.txt
